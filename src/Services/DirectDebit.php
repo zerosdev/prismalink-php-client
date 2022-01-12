c60c8339767eff3acb307d28ae399cbc8d6dd836
ZerosDev <ronywisnuwardana@gmail.com>
2022-01-12 18:12:27 +0700

    add direct debit

--- src/Services/DirectDebit.php
+++ src/Services/DirectDebit.php
@@ -34,4 +34,62 @@ class DirectDebit
 
         return $this->client->request('debitin/directdebitrequest', 'POST');
     }
+
+    public function confirmation($session_token)
+    {
+        $payload = $this->defined_properties;
+
+        $payload = array_merge($payload, [
+            'transmission_date_time' => date('Y-m-d H:i:s.v O'),
+        ]);
+
+        $this->client->setRequestPayload($payload);
+
+        $jsonPayload = json_encode($this->client->getRequestPayload());
+
+        $mac = hash_hmac('sha256', $jsonPayload, $this->client->getSecretKey());
+
+        $this->client->addRequestHeaders('MAC', $mac);
+        $this->client->addRequestHeaders('session_token', $session_token);
+
+        return $this->client->request('debitin/directdebitconfirmation', 'POST');
+    }
+
+    public function unbind()
+    {
+        $payload = $this->defined_properties;
+
+        $payload = array_merge($payload, [
+            'transmission_date_time' => date('Y-m-d H:i:s.v O'),
+        ]);
+
+        $this->client->setRequestPayload($payload);
+
+        $jsonPayload = json_encode($this->client->getRequestPayload());
+
+        $mac = hash_hmac('sha256', $jsonPayload, $this->client->getSecretKey());
+
+        $this->client->addRequestHeaders('MAC', $mac);
+
+        return $this->client->request('debitin/unbind', 'POST');
+    }
+
+    public function changeLimit()
+    {
+        $payload = $this->defined_properties;
+
+        $payload = array_merge($payload, [
+            'transmission_date_time' => date('Y-m-d H:i:s.v O'),
+        ]);
+
+        $this->client->setRequestPayload($payload);
+
+        $jsonPayload = json_encode($this->client->getRequestPayload());
+
+        $mac = hash_hmac('sha256', $jsonPayload, $this->client->getSecretKey());
+
+        $this->client->addRequestHeaders('MAC', $mac);
+
+        return $this->client->request('debitin/change-limit', 'POST');
+    }
 }
